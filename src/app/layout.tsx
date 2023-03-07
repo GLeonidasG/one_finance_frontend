148d076a5e595fd95898860154952ee0f1fb6732
Leonidas Goetz <genariolgoetz99@gmail.com>
2023-03-07 20:41:04 -0300

    feat: setup tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='h-full max-w-md mx-auto '>
       <body>{children}</body>
     </html>
   )
