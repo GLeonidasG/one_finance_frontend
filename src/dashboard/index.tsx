510eb788dd24a519c94bb843427d72ec9bea70fc
Leonidas Goetz <leonidas.goetz@sap.com>
2023-03-28 20:01:21 -0300

    refactor: use set instead of array

--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -43,6 +43,7 @@ const cards = [
 type RecordType = "CREDIT" | "DEBIT";
 
 type Record = {
+  id?: number,
   title: string,
   description: string,
   value: number,
@@ -50,13 +51,16 @@ type Record = {
   entryDate: string
 }
 
-const records: Record[] = [
-  { title: "UNISIONS", description: "Mensalidade da UNISIONS", value: 450, type: "DEBIT", entryDate: new Date().toISOString() },
-  { title: "SALARIO", description: "Entrada do salario", value: 9500, type: "CREDIT", entryDate: new Date().toISOString() },
-  { title: "Quiropraxia", description: "Quiropraxia", value: 310, type: "DEBIT", entryDate: new Date().toISOString() },
-  { title: "Vale Ali/Ref", description: "Entrada dos vales", value: 850, type: "CREDIT", entryDate: new Date().toISOString() }
-]
+const records = new Map<number, Record>()
 
+for (const record of [
+  { id: 0, title: "UNISIONS", description: "Mensalidade da UNISIONS", value: 450, type: "DEBIT", entryDate: new Date().toLocaleDateString() },
+  { id: 1, title: "SALARIO", description: "Entrada do salario", value: 9500, type: "CREDIT", entryDate: new Date().toLocaleDateString() },
+  { id: 2, title: "Quiropraxia", description: "Quiropraxia", value: 310, type: "DEBIT", entryDate: new Date().toLocaleDateString() },
+  { id: 3, title: "Vale Ali/Ref", description: "Entrada dos vales", value: 850, type: "CREDIT", entryDate: new Date().toLocaleDateString() }
+] as Record[]) {
+  records.set(record.id || 0, record);
+}
 
 type FormModalAction = "CREATE" | "UPDATE";
 type OnSubmitResult = { action: FormModalAction, value: Record }
@@ -69,13 +73,12 @@ type RecordFormModalProps = {
   data?: Record | null
 };
 
-const EmptyRecord: Record = { title: "", description: "", entryDate: new Date().toISOString().substring(0, 10), type: "CREDIT", value: 0 };
+const EmptyRecord: Record = { title: "", description: "", entryDate: new Date().toLocaleDateString().substring(0, 10), type: "CREDIT", value: 0 };
 function RecordFormModal({ showModal, onSubmit, onCancel, action, data }: RecordFormModalProps) {
 
   const [record, setRecord] = useState<Record>(data || EmptyRecord);
 
   useEffect(() => {
-    console.log(data)
     if (data) setRecord(data)
   }, [data])
 
@@ -98,9 +101,34 @@ function RecordFormModal({ showModal, onSubmit, onCancel, action, data }: Record
   ) : null
 }
 
+type ConfirmationModalProps = {
+  showModal: boolean,
+  onConfirm: () => void,
+  onCancel: () => void,
+  title: string,
+  body: string,
+  confirmText: string,
+  cancelText: string
+};
+
+function ConfirmationModal({ showModal, onConfirm, onCancel, title, body, confirmText, cancelText }: ConfirmationModalProps) {
+  return showModal ? (
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-gray-900 opacity-90 backdrop-blur-3xl">
+      <div className="flex flex-col p-8 font-bold text-black bg-gray-700 rounded-lg opacity-100 w-80">
+        <h1 className="my-2 font-sans text-xl font-semibold tracking-wide text-center text-white">{title}</h1>
+        <p className="my-1 font-sans text-base font-normal tracking-normal text-center text-white">{body}</p>
+        <div className="flex justify-between flex-1 flex-rol">
+          <button className="mx-2 my-2 text-white w-28" onClick={() => onConfirm()}>{confirmText}</button>
+          <button className="mx-2 my-2 text-white w-28" onClick={() => onCancel()}>{cancelText}</button>
+        </div>
+      </div>
+    </div>
+  ) : null
+}
+
 
 type DashboardTableProps = {
-  records: Record[];
+  records: Map<number, Record>;
   onClick?: (index: number) => void;
   onDelete?: (index: number) => void;
 };
@@ -108,7 +136,7 @@ type DashboardTableProps = {
 function DashboardTable({ records, onClick, onDelete }: DashboardTableProps) {
   const headers = Object.keys(EmptyRecord || {});
   return (
-    <div className="w-full p-2 overflow-auto bg-gray-900 rounded-lg shadow-md h-min">
+    <div className="w-full h-full p-2 overflow-auto bg-gray-900 rounded-lg shadow-md">
       <table className="w-full border border-collapse rounded-lg border-slate-500">
         <thead className="bg-gray-900 border-b-1 border-b-gray-50">
           <tr>
@@ -116,19 +144,19 @@ function DashboardTable({ records, onClick, onDelete }: DashboardTableProps) {
           </tr>
         </thead>
         <tbody>
-          {records.map((record, recordIndex) =>
+          {[...records?.values()].map((record, recordIndex) =>
             <tr key={recordIndex} className={`even:bg-gray-800 odd:bg-gray-900`}>
               {headers.map((header, headerIndex) =>
                 <>
                   <td
-                    onClick={() => !onClick ? () => { } : onClick(recordIndex)}
+                    onClick={() => !onClick ? () => { } : onClick(record.id || 0)}
                     key={`${recordIndex}-${headerIndex}`}
-                    className="border border-slate-700 hover:cursor-pointer">{(record as any)[header]}
+                    className="pl-2 border border-slate-700 hover:cursor-pointer">{(record as any)[header]}
                   </td>
                 </>
               )}
               <td
-                onClick={() => !onDelete ? () => { } : onDelete(recordIndex)}
+                onClick={() => !onDelete ? () => { } : onDelete(record.id || 0)}
                 key={`${recordIndex}-delete`}
                 className="font-mono text-2xl font-bold text-center border border-slate-700 hover:cursor-pointer"
               >
@@ -144,41 +172,68 @@ function DashboardTable({ records, onClick, onDelete }: DashboardTableProps) {
 
 export default function Dashboard() {
   const [showModal, setShowModal] = useState(false);
-  const [localRecords, setLocalRecords] = useState(records)
+  const [showConfirmation, setShowConfimation] = useState(false)
+  const [localRecords, setLocalRecords] = useState<typeof records>(records)
   const [recordToUpdate, setRecordToUpdate] = useState<Record | null>(null)
   const [lastIndex, setLastIndex] = useState<number | null>(null)
 
   const updateRecords = (record: Record) => {
-    console.log(lastIndex)
     if (lastIndex !== null) {
-      console.table({ lastIndex, record })
-      localRecords[lastIndex] = record
+      localRecords.set(lastIndex, { ...localRecords.get(lastIndex), ...record })
       setLocalRecords(localRecords)
       setRecordToUpdate(null)
     }
   }
 
   const createRecords = (record: Record) => {
-    setLocalRecords([...localRecords, record])
+    const nextId = ([...localRecords.values()].sort((a, b) => (a.id as number) - (b.id as number)).pop()?.id || 0) + 1;
+    localRecords.set(nextId, { ...record, id: nextId })
+    setLocalRecords(localRecords);
   }
 
 
   return (
     <div className="flex flex-1 m-6">
+      <ConfirmationModal
+        onConfirm={() => {
+          if (lastIndex !== null) {
+            console.log(...localRecords.values());
+            localRecords.delete(lastIndex);
+            console.log(...localRecords.values());
+            setLocalRecords(localRecords);
+          }
+          setShowConfimation(false);
+        }}
+        onCancel={() => {
+          setLastIndex(null);
+          setShowConfimation(false);
+        }}
+        title="Delete record"
+        body="You are about to remove a record from the list. Are you sure?"
+        confirmText="Yes"
+        cancelText="No"
+        showModal={showConfirmation}
+      />
       <RecordFormModal
-        onCancel={() => setShowModal(false)}
+        onCancel={() => {
+          setRecordToUpdate(null);
+          setLastIndex(null);
+          setShowModal(false);
+        }}
         showModal={showModal}
         data={recordToUpdate}
         action={recordToUpdate ? "UPDATE" : "CREATE"}
         onSubmit={({ value: record, action }) => {
-          console.table({ record, action })
-          if (action === "CREATE") createRecords(record)
-          if (action === "UPDATE") updateRecords(record)
-          setShowModal(false)
+          console.table({ record, action });
+          if (action === "CREATE") createRecords(record);
+          if (action === "UPDATE") updateRecords(record);
+          setShowModal(false);
         }}
       />
       <div className="flex flex-col items-center flex-1 max-w-md p-4 m-2 overflow-scroll bg-gray-800 shadow-md rounded-xl opacity-90 backdrop:blur-lg">
-        {cards.map((card) => <Card {...card} />)}
+        {cards.map((card) => (
+          <Card {...card} />
+        ))}
       </div>
       <div className="flex flex-col flex-1 p-3 bg-gray-800 rounded-xl backdrop:blur-lg opacity-90 table-container">
         <div className="flex flex-1 mb-5 max-h-12">
@@ -186,12 +241,13 @@ export default function Dashboard() {
         </div>
         <DashboardTable
           onClick={(index) => {
-            setRecordToUpdate(localRecords[index])
-            setLastIndex(index)
-            setShowModal(true)
+            setRecordToUpdate(localRecords.get(index) || null);
+            setLastIndex(index);
+            setShowModal(true);
           }}
           onDelete={(index) => {
-            setLocalRecords(localRecords.filter((_, recordIndex) => recordIndex !== index))
+            setLastIndex(index);
+            setShowConfimation(true);
           }}
           records={localRecords}
         />
