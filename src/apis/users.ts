84fb1d0f78557dc0af702b3b687ea36de002db7a
Leonidas Goetz <leonidas.goetz@sap.com>
2023-04-04 22:33:28 -0300

    feat: add API integration for users

---
+++ src/apis/users.ts
@@ -0,0 +1,14 @@
+import { Card } from "./cards";
+export const BASE_URL = "http://localhost:3000";
+  
+
+export class User {
+  constructor(
+    public username: string,
+    public email: string,
+    public cards: Array<Card>
+  ) { }
+}
+export async function getUsers(id: number): Promise<User> {
+  return await (await fetch(new URL(`${BASE_URL}/user/${id}/withCards`))).json();
+}
