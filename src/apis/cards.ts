8597cd7813e7becec8bfd799f927f565c73bb97e
Leonidas Goetz <leonidas.goetz@sap.com>
2023-04-07 12:47:00 -0300

    feat: add API integration for create update and delete

--- src/apis/cards.ts
+++ src/apis/cards.ts
@@ -1,3 +1,5 @@
+import {BASE_URL} from "./users"
+
 export const getYearMonth = (date: string | Date) =>
   new Date(date)
     .toLocaleDateString()
@@ -18,3 +20,28 @@ export class Card {
   public get _validFrom() { return getYearMonth(this.validFrom) }
 }
 
+
+export async function createCard(card: Card): Promise<void> {
+  const options = {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(card)
+  }
+  await fetch(`${BASE_URL}/card`, options)
+}
+
+export async function updateCard(id: string | number, card: Card): Promise<void> {
+  const options = {
+    method: "PATCH",
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(card)
+  }
+  await fetch(`${BASE_URL}/card/${id}`, options)
+}
+
+export async function deleteCard(id: string | number): Promise<void> {
+  const options = {
+    method: "DELETE",
+  }
+  await fetch(`${BASE_URL}/card/${id}`, options)
+}
